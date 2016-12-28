ae3a09c5177f45ef9e857b62bb6b54c413af73c4
nut <xd.ziitun@hotmail.co.kr>
2016-12-28 13:49:26 +0700

    add new text reply

--- bot.php
+++ bot.php
@@ -18,8 +18,13 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				{'type' => 'text',
+				 'text' => $text
+				},
+				{
+				 'type' => 'text',
+				 "Yo! What's up. How can I help you?" => $text
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
